e4d6f775554648e9adc41c7b4babfaad585a3e81
NethajiChowdary <nethajichowdary555@gmail.com>
2021-06-28 07:30:03 +0530

    Added: Code to Create Board

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,5 +3,20 @@ class TicTacToe
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to TicTacToe Game");
+		Board();
 	}
+
+public static void Board()
+   {
+    char[] board=new char[8];
+    for(int i=0;i<=2;i++)
+    {
+        for(int j=0;j<=2;j++)
+        {
+            System.out.print('_');
+            System.out.print('|');
+        }
+        System.out.println();
+    }
+   }
 }
