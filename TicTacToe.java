b6ad7c2e0354c50382a7581adc749a1a656410c6
NethajiChowdary <nethajichowdary555@gmail.com>
2021-06-28 07:43:03 +0530

    Added: Code to Display Board

--- TicTacToe.java
+++ TicTacToe.java
@@ -16,8 +16,15 @@ class TicTacToe
      {
 
         char[] board =new char[8];
+	for(int i=0;i<=2;i++)
+        {
+          for(int j=0;j<=2;j++)
+          {
+                  System.out.print('-');
+                  System.out.print('|');
+           }
               System.out.println();
-
+	}
      }
 
          public static void  chooseLetter(char letter)
